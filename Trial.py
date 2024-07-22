8e467f4936dbe3ad652077ea62184ac51e600aab
docube <segundavids091@gmail.com>
2024-07-22 15:07:36 +0100

    commit

---
+++ Trial.py
@@ -0,0 +1,10 @@
+my_list = ['STRING', 100,23.2]
+print(len(my_list))
+
+mylist = ['one', 'two', 'three']
+print(mylist[0])
+print(my_list[1:])
+print(mylist[1:])
+another_list = ['four', 'five']
+new_list = my_list + another_list
+print(new_list)
